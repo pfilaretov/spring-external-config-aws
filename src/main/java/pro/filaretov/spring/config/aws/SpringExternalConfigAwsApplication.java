e64b7910b27a28b57b336bcbc5fb1ee0160d3c37
Petr Filaretov <petr.filaretov@gmail.com>
2021-09-02 14:26:47 +0300

    initial commit

---
+++ src/main/java/pro/filaretov/spring/config/aws/SpringExternalConfigAwsApplication.java
@@ -0,0 +1,13 @@
+package pro.filaretov.spring.config.aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringExternalConfigAwsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringExternalConfigAwsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/filaretov/spring/config/aws/SpringExternalConfigAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.filaretov.spring.config.aws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringExternalConfigAwsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
